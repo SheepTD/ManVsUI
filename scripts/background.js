7808fc54c463837dc6e5d5db0dc51c2414f4e6ef
Seán Osborne <123533598+BitBoyTD@users.noreply.github.com>
2023-09-28 09:39:51 +1300

    Add files via upload

--- scripts/background.js
+++ scripts/background.js
@@ -1,41 +1,43 @@
+// set the default settings on installation
 chrome.runtime.onInstalled.addListener(() => {
     chrome.storage.sync.set({ extensionEnabled: false });
     chrome.storage.sync.set({ mode: 'dark' })
 });
 
-// website
+// website to check for
 const manvsmaths = "https://manvsmaths.com/";
-// different sets of navigation buttons
+// different sets of navigation buttons for each of the different pages
 const juniorNavArray = ['Main', 'Resources', 'Algebra', 'Videos', 'Online', 'Home'];
 const yr11NavArray = ['Main', 'Externals', 'Internals', 'Videos', 'Algebra', 'Home'];
 const yr12yr13cNavArray = ['Main', 'Externals', 'Internals', 'Videos', 'Home'];
 const yr13sNavArray = ['Main', 'Externals', 'Internals', 'Home'];
 const teachersNavArray = ['Main', 'Links', 'Matrix', 'Home'];
 const otherNavArray = ['Home'];
-// send message to content.js
+// send message to content.js with information about what to display
 function msgContentJs(sendToTab, url, navArray, fileSysValOne, fileSysValTwo) {
     chrome.tabs.sendMessage(sendToTab, {url: url, navArray: navArray, fileSysValOne: fileSysValOne, fileSysValTwo: fileSysValTwo});
 }
-
+// activates every time the a page loads
 chrome.webNavigation.onCompleted.addListener((details) => {
     // check if page loaded is in the main frame
     if (details.frameId === 0) {
         // variables we'll need
         const url = details.url;
+        // ping the url to make it easier to fix any errors
         console.log(url);
         const tabId = details.tabId;
         let navArray = 'none';
         let fileSysValOne = 'none';
         let fileSysValTwo = 'none'
-        // check if it's manvsmaths
+        // check if the page is a manvsmaths page, but isn't a glitchd page, the home page, the error page or a pdf file
         if (url.startsWith(manvsmaths) && url !== "https://manvsmaths.com/" && url !== 'https://manvsmaths.com/home.html' && url !== 'https://manvsmaths.com/404.html' && url.endsWith('.pdf') !== true) {
-            // grab the current settings
+            // grab the current settings from storage
             chrome.storage.sync.get(['extensionEnabled', 'mode'], function (data) {
                 const extensionEnabled = data.extensionEnabled;
                 const mode = data.mode;
-                // check if it's turned on
+                // check if the extension is turned on
                 if (extensionEnabled === true) {
-                    // which styles do we need to apply?
+                    // find which page it is via the url
                     let page = url.split('v')[1].split('/')[1];
                     if (page === 'teachers' || page === 'main') {
                         page = 'other';
@@ -52,11 +54,8 @@ chrome.webNavigation.onCompleted.addListener((details) => {
                             files: ["styles/"+page+"-dark-styles.css"],
                         });
                     }
-                    // find which page we are on
-                    if (url.startsWith(manvsmaths + "home")) {
-                        fileSysValOne = 'Home';
-                        fileSysValTwo = '';
-                    } else if (url.startsWith(manvsmaths + "09")) {
+                    // find which page we are on in order to set up the file system
+                    if (url.startsWith(manvsmaths + "09")) {
                         fileSysValOne = 'Year 9'
                         navArray = juniorNavArray;
                         fileSysValTwo = url.split('_')[1].split('.')[0].charAt(0).toUpperCase() + url.split('_')[1].split('.')[0].slice(1);
@@ -75,12 +74,10 @@ chrome.webNavigation.onCompleted.addListener((details) => {
                     } else if (url.startsWith(manvsmaths + "13c")) {
                         fileSysValOne = 'Calculus'
                         navArray = yr12yr13cNavArray;
-                        /* https://manvsmaths.com/13c/13_calculusexternals.html */
                         fileSysValTwo = url.split('_')[1].slice(8).split('.')[0].charAt(0).toUpperCase() + url.split('_')[1].slice(8).split('.')[0].slice(1);
                     } else if (url.startsWith(manvsmaths + "13s")) {
                         fileSysValOne = 'Statistics'
                         navArray = yr13sNavArray;
-                        /* https://manvsmaths.com/13s/13_statisticsmain.html */
                         fileSysValTwo = url.split('_')[1].slice(10).split('.')[0].charAt(0).toUpperCase() + url.split('_')[1].slice(10).split('.')[0].slice(1);
                     } else if (url.startsWith(manvsmaths + "teachers")) {
                         fileSysValOne = 'Teachers'
@@ -99,6 +96,7 @@ chrome.webNavigation.onCompleted.addListener((details) => {
                         navArray = otherNavArray;
                         fileSysValTwo = 'Main';
                     }
+                    // send message to content.js with info about the page
                     msgContentJs(tabId, url, navArray, fileSysValOne, fileSysValTwo); 
                 }
             });
--- scripts/content.js
+++ scripts/content.js
@@ -6,50 +6,39 @@ function remEl(el) {
         parent.removeChild(elArray[index]);
     });
 }
-// Get ready to rumble!!!!
+// listen for message from background.js
 chrome.runtime.onMessage.addListener(function (msg) {
-    // add tag to allow for font changing
-    const metaTag = document.createElement('meta');
-    metaTag.setAttribute('http-equiv', 'Content-Security-Policy');
-    metaTag.setAttribute('content', 'upgrade-insecure-requests');
-    document.querySelector('head').appendChild(metaTag);
-    // ping msg recieved
+    // ping msg recieved to the console
     console.log("recieved: " + msg.fileSysValOne + "/" + msg.fileSysValTwo);
-    // set up the variables
+    // set up variables
     const url = msg.url;
     const navArray = msg.navArray;
     const fileSysValOne = msg.fileSysValOne;
     const fileSysValTwo = msg.fileSysValTwo;
-    // remove stylesheet and other elements
+    // remove stylesheet and other elements using the remove element function
     remEl('link');
     remEl('.overlay');
     remEl('.logo');
     remEl('.button');
     remEl('#buttons br');
-    // fix image overflow
-    if (url === "https://manvsmaths.com/13c/13_calculusmain.html") {
-        document.querySelectorAll('.maintext')[2].setAttribute('id', "imgOverflow");
-    }
-    // replace nav buttons and set up nav links div
+    // set up nav links div in place of the old nav buttons
     const buttonsDiv = document.getElementById('buttons');
-    // sort out the nav links
     const navLinksDiv = document.createElement('div');
     navLinksDiv.id = 'navLinks';
     buttonsDiv.appendChild(navLinksDiv);
     const navLinkArray = document.querySelectorAll('#buttons a');
     navLinkArray.forEach(function(zilch, index) {
         navLinkArray[index].innerHTML = navArray[index];
-        // add the nav links div
         navLinksDiv.appendChild(navLinkArray[index]);
     });
-    // create file system
+    // create the file system
     const fileSys = document.createElement('div');
     fileSys.id = 'fileSys';
     const mvm = document.createElement('a');
     mvm.innerHTML = 'ManVsMath > ';
     mvm.href = 'https://manvsmaths.com/home.html'
     fileSys.appendChild(mvm);
-    //val one
+    // set up value one
     const valOne = document.createElement('a');
     valOne.innerHTML = fileSysValOne + ' > ';
     // sort out the href for fileSysValOne
@@ -68,16 +57,22 @@ chrome.runtime.onMessage.addListener(function (msg) {
         valOne.href = before_ + '_main.html';
     }
     fileSys.appendChild(valOne);
-    // val two
+    // set up val two
     const valTwo = document.createElement('a');
     valTwo.innerHTML = fileSysValTwo;
     valTwo.href = url;
     fileSys.appendChild(valTwo);
     buttonsDiv.appendChild(fileSys);
-    // fix the table backgrounds
-    const tdArray = document.querySelectorAll('td');
+    // fix image overflow issue on 13 calculus main page
+    if (url === "https://manvsmaths.com/13c/13_calculusmain.html") {
+            document.querySelectorAll('.maintext')[2].setAttribute('id', "imgOverflow");
+    }
+    // fix the table backgrounds on teachers matrix page
+    if (url === "https://manvsmaths.com/teachers/teachersmatrix.html") {
+            const tdArray = document.querySelectorAll('td');
     for (let i = 0; i <= tdArray.length; i ++) {
         const el = tdArray[i];
         el.setAttribute('style', 'none');
+    }
     }
 });
